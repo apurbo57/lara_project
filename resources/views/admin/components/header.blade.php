97ce2a36f3544c36c4ac271e5128e599ee3c67ef
Anowar Hossain Apurbo <ahosien57@gmail.com>
2021-10-14 17:50:10 +0600

    index

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -5,7 +5,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link {{ request()->is('admin/dashboard')?'active':'' }}" href="{{ route('index') }}">Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link {{ request()->is('admin/dashboard')?'active':'' }}" href="{{ route('admin.dashboard') }}">Dashboard</a></li>
                 <li class="nav-item"><a class="nav-link {{ request()->is('admin/category')?'active':'' }}" href="{{ route('admin.category.index') }}">Category</a></li>
                 
                 
